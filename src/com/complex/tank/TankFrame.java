0058428923117c480c55d719ca88e7680f48a69d
ZWJ <1176566869@qq.com>
2021-01-18 17:09:08 +0800

    从Frame类继承

---
+++ src/com/complex/tank/TankFrame.java
@@ -0,0 +1,24 @@
+package com.complex.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame {
+    public TankFrame(){
+        this.setSize(800,600);
+        this.setResizable(false);
+        this.setTitle("tank war");
+        this.setVisible(true);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+    @Override
+    public void paint(Graphics g){
+        g.fillRect(200,200,50,50);
+    }
+}
