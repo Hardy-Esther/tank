ab8dfca2d592cce7f6f36ce056658c402d9dc0a3
ZWJ <1176566869@qq.com>
2021-01-19 20:48:22 +0800

    解决图片闪烁问题

--- src/com/complex/tank/TankFrame.java
+++ src/com/complex/tank/TankFrame.java
@@ -11,8 +11,10 @@ public class TankFrame extends Frame {
     Tank myTank = new Tank(200,200,Dir.DOWN);
     Bullet myBullet = new Bullet(300,300,Dir.DOWN);
 
+    private final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
+
     public TankFrame() {
-        this.setSize(800, 600);
+        this.setSize(GAME_WIDTH, GAME_HEIGHT);
         this.setResizable(false);
         this.setTitle("tank war");
         this.setVisible(true);
@@ -23,6 +25,21 @@ public class TankFrame extends Frame {
             }
         });
         this.addKeyListener(new MyKeyListener());
+    }
+
+    Image offScreenImage = null;
+    @Override
+    public void update(Graphics g){
+        if (offScreenImage == null){
+            offScreenImage = this.createImage(GAME_WIDTH,GAME_HEIGHT);
+        }
+        Graphics gOffScreen = offScreenImage.getGraphics();
+        Color c = gOffScreen.getColor();
+        gOffScreen.setColor(Color.BLACK);
+        gOffScreen.fillRect(0,0,GAME_WIDTH,GAME_HEIGHT);
+        gOffScreen.setColor(c);
+        paint(gOffScreen);
+        g.drawImage(offScreenImage,0,0,null);
     }
 
     @Override
