b821aaf9e80ec03dac518ffc869f5dc05f14e567
ZWJ <1176566869@qq.com>
2021-01-21 16:52:57 +0800

    增加配置文件

--- src/com/complex/tank/Main.java
+++ src/com/complex/tank/Main.java
@@ -8,11 +8,12 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         TankFrame tf = new TankFrame();
         //初始化敌方tank
-        for (int i = 0; i < 5; i++) {
-            tf.tanks.add(new Tank(50+i*150,200,Dir.DOWN,tf));
+        int initTankCount = Integer.parseInt((String) PropertyMgr.get("initTankCount"));
+        for (int i = 0; i < initTankCount; i++) {
+            tf.tanks.add(new Tank(50 + i * 150, 200, Dir.DOWN, tf));
         }
 
-        while (true){
+        while (true) {
             Thread.sleep(50);
             tf.repaint();
         }
